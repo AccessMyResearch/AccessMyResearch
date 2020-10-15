ab2b4699545b9fc2a73fb400e1d40db07523fa26
Caleb Hoff <crunchycat1@gmail.com>
2020-10-14 20:44:57 -0500

    moved article icon

--- src/views/Tables/LightTable.vue
+++ src/views/Tables/LightTable.vue
@@ -40,14 +40,14 @@
                              prop="name">
                 <template v-slot="{row}">
                     <b-media no-body class="align-items-center">
-                        <a href="#" class="mr-3">
-                            <b-img class="avatar" rounded="circle" alt="Image placeholder" :src="row.img" />
-                        </a>
                         <b-media-body>
                             <span style="font-family:Verdana" class="font-weight-800 name mb-0 text-sm">{{row.name}}</span>
                         </b-media-body>
                     </b-media>
                     <b-media no-body class="align-items-center">
+                        <a href="#" class="mr-3">
+                            <b-img class="avatar" rounded="circle" alt="Image placeholder" :src="row.img" />
+                        </a>
                         <p class="font-weight-400 name mb-0 text-black"> Author(s): 
                             <span class="font-weight-700 name mb-0 text-blue">{{row.author}}</span>
                         </p>
